8cfb58278b20786ec8d594dd7bea92a2723bc88f
Steve Kondik <shade@chemlab.org>
2009-07-24 09:57:16 -0400

    Patch from irrenhaus@XDA for autoclose of folders when launching apps.

---
+++ src/com/android/launcher/MenuAddFolder.java
@@ -0,0 +1,16 @@
+package com.android.launcher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuAddFolder extends Activity {
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        
+        
+    }
+
+}
